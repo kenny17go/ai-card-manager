a66d0f04cf5cb3acb6478e48f70d195091d09f80
kenny17go <kenny0217@ymail.com>
2026-08-20 22:53:14 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-const C='ai-card-v021';const A=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(x=>Promise.all(x.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+```javascript
+const CACHE = 'card-v6';
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['index.html', 'style.css', 'app.js', 'manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+```
